b03751c53f9c123f409081849fbb3765a4328ce9
jotaaa22 <joniangamarca333@gmail.com>
2025-01-15 17:12:49 +0100

    Updated

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ lectura_csv = pd.read_csv("filmatch.csv..csv",
 @app.route('/', methods=['GET', 'POST'])
 def index():
     resultado = None
-    plataformas_csv = ['Netflix', 'Hulu', 'Prime Video', 'Disney+']
     plataformas_pelicula = None
     mensaje_error = None
 
@@ -19,33 +18,29 @@ def index():
         if 'plataforma' in request.form:
             # Obtener la plataforma seleccionada
             plataforma = request.form['plataforma'].strip().title()
+            
+            # Filtrar las películas disponibles en la plataforma seleccionada
+            peliculas_en_plataforma = lectura_csv[lectura_csv['streaming_service'].str.contains(plataforma, case=False, na=False)]
 
-            # Validar la plataforma
-            if plataforma in plataformas_csv:
-                # Filtrar las películas disponibles en la plataforma seleccionada
-                peliculas_en_plataforma = lectura_csv[lectura_csv[plataforma] == 1]
-
-                # Ordenar las películas por valoración en orden descendente
-                orden_peliculas = peliculas_en_plataforma.sort_values(by='Valoracion', ascending=False)
+            if not peliculas_en_plataforma.empty:
+                # Ordenar las películas por IMDb score en orden descendente
+                orden_peliculas = peliculas_en_plataforma.sort_values(by='imdb_score', ascending=False)
 
                 # Obtener las 10 mejores películas
                 resultado = orden_peliculas.head(10).to_dict(orient='records')
             else:
-                mensaje_error = 'Plataforma no válida. Por favor, escoja entre Netflix, Hulu, Prime Video, Disney+.'
+                mensaje_error = f"No se encontraron películas en la plataforma '{plataforma}'."
 
         if 'titulo' in request.form:
             # Obtener el título de la película
             titulo_pelicula = request.form['titulo'].strip()
 
             # Buscar la película
-            pelicula_encontrada = lectura_csv[lectura_csv['Title'].str.contains(titulo_pelicula, case=False, na=False)]
+            pelicula_encontrada = lectura_csv[lectura_csv['title'].str.contains(titulo_pelicula, case=False, na=False)]
 
             if not pelicula_encontrada.empty:
-                # Encontrar las plataformas donde está disponible
-                plataformas_pelicula = []
-                for plataforma in plataformas_csv:
-                    if pelicula_encontrada[plataforma].any():
-                        plataformas_pelicula.append(plataforma)
+                # Obtener las plataformas donde está disponible
+                plataformas_pelicula = pelicula_encontrada['streaming_service'].unique().tolist()
             else:
                 mensaje_error = f"No se encontró ninguna película con el título '{titulo_pelicula}'."
 
